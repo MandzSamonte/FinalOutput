5c0c9f75bb779d49b9581d67d022495d12c63bcd
John Mandy Samonte <johnmandysamonte@gmail.com>
2020-05-14 15:51:56 +0800

    Final Output In Progress

--- FinalOutput.java
+++ FinalOutput.java
@@ -4,6 +4,9 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,7 +20,10 @@ import java.util.Scanner;
 
 class FinalOutput extends JFrame {
 
-    int total = 0;
+    public static String auth;
+
+    public static int TOTAL = 0;
+
 
     public FinalOutput(){
         setVisible(false);
@@ -34,7 +40,7 @@ class FinalOutput extends JFrame {
         tableModel.setColumnIdentifiers(productColumnIdentifiers);
 
         DefaultTableModel customerTableModel = new DefaultTableModel();
-        String[] recieptColumnIdentifiers = {"Name","Price", "QTY"};
+        String[] recieptColumnIdentifiers = {"Name","Price"};
         customerTableModel.setColumnIdentifiers(recieptColumnIdentifiers);
 
         JTable tblProductList = new JTable(tableModel);
@@ -88,13 +94,12 @@ class FinalOutput extends JFrame {
         tblProductList.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-
                 if(e.getValueIsAdjusting()){
                     customerTableModel.addRow(tableModel.getDataVector().elementAt(tblProductList.getSelectedRow()));
 
-                    total += Integer.parseInt((String)customerTableModel.getValueAt(customerTableModel.getRowCount() - 1, 1));
-                    lblTotal.setText(Integer.toString(total));
-                    System.out.println("total: " + total);
+                    TOTAL += Integer.parseInt((String)customerTableModel.getValueAt(customerTableModel.getRowCount() - 1, 1));
+                    lblTotal.setText(Integer.toString(TOTAL));
+                    System.out.println("total: " + TOTAL);
                 }
             }
         });
@@ -110,32 +115,120 @@ class FinalOutput extends JFrame {
         };
         setLayout(gridBagLayout);
 
-        JPanel container = new JPanel();
-        container.setLayout(new FlowLayout(FlowLayout.LEFT));
-        container.add(new JButton("Sample"));
-        container.add(lblTotal);
+        JPanel printContainer = new JPanel();
+        JButton printReceipt = new JButton("Print Receipt");
+        printReceipt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RecieptFrame recieptFrame = new RecieptFrame();
+                RecieptFrame.receiptTableModel.setRowCount(0);
+                for(int i = 0; i < customerTableModel.getRowCount(); i++){
+                    RecieptFrame.receiptTableModel.addRow(customerTableModel.getDataVector().elementAt(i));
+                }
+                customerTableModel.setRowCount(0);
 
+                recieptFrame.setVisible(true);
+            }
+        });
+        printContainer.setLayout(new FlowLayout(FlowLayout.LEFT));
+        printContainer.add(printReceipt);
+        printContainer.add(lblTotal);
 
+        JButton voidButton = new JButton("Void");
+        voidButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(auth);
+                VoidItem voidItem = new VoidItem();
+                voidItem.setVisible(true);
+            }
+        });
+        JPanel addItemContainer = new JPanel();
+        addItemContainer.setLayout(new FlowLayout(FlowLayout.LEFT));
+        addItemContainer.add(voidButton);
+
+        // Layout
         gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
         gridBagConstraints.insets = new Insets(20, 20, 20, 20);
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridx = 0;
         add(cbbProductType, gridBagConstraints);
+        gridBagConstraints.gridy = 1;
+        add(new JScrollPane(tblProductList), gridBagConstraints);
+        gridBagConstraints.gridy = 2;
+        add(addItemContainer, gridBagConstraints);
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
         add(new JLabel("Sample"), gridBagConstraints);
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        add(new JScrollPane(tblProductList), gridBagConstraints);
-        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         add(new JScrollPane(tblCustomerOrder), gridBagConstraints);
-        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
-        add(container, gridBagConstraints);
+        add(printContainer, gridBagConstraints);
+    }
+
+    public static class VoidItem extends JFrame {
+        public VoidItem() {
+            setVisible(false);
+            setSize(500, 300);
+            setLocationRelativeTo(null);
+            setDefaultCloseOperation(HIDE_ON_CLOSE);
+            setLayout(new GridBagLayout());
+
+            // Components
+            JLabel lblUserName = new JLabel("Username: ");
+            JTextField inpUsername = new JTextField();
+            inpUsername.setPreferredSize(new Dimension(300, 40));
+            lblUserName.setLabelFor(inpUsername);
+
+            JLabel lblPassword = new JLabel("Password: ");
+            JPasswordField inpPassword = new JPasswordField();
+            inpPassword.setPreferredSize(new Dimension(300, 40));
+            lblPassword.setLabelFor(inpPassword);
+
+            JButton SubmitButton = new JButton("Void");
+            SubmitButton.setPreferredSize(new Dimension(300, 40));
+
+            // Functions
+            SubmitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(!inpUsername.getText().isEmpty() && !inpPassword.getText().isEmpty()){
+                        try {
+                            Scanner adminList = new Scanner(new FileReader("/Users/johnmandysamonte/Library/Preferences/IdeaIC2019.3/scratches/FinalOutput/adminList.txt"));
+                            while(adminList.hasNext()){
+                                String name = adminList.next();
+                                String password = adminList.next();
+
+                                if(name.equals(inpUsername.getText()) && password.equals(new String(inpPassword.getPassword()))){
+                                    System.out.println("VOIDED");
+                                    break;
+                                }
+                            }
+                        } catch (FileNotFoundException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+            // Layouts
+            GridBagConstraints gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.gridy = 0;
+            add(lblUserName, gridBagConstraints);
+            gridBagConstraints.gridy = 1;
+            add(inpUsername, gridBagConstraints);
+            gridBagConstraints.gridy = 2;
+            add(lblPassword, gridBagConstraints);
+            gridBagConstraints.gridy = 3;
+            add(inpPassword, gridBagConstraints);
+            gridBagConstraints.gridy = 4;
+            add(SubmitButton, gridBagConstraints);
+        }
     }
 
     public static class LoginFrame extends JFrame {
+        public String auth;
+
         public LoginFrame(){
             setVisible(true);
             setSize(500, 300);
@@ -198,12 +291,15 @@ class FinalOutput extends JFrame {
             });
 
 
+
+
             btnLogin.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     switch (cbbLoginType.getSelectedIndex()){
                         case 0:
                             if(!txfCounterInput.getText().isEmpty()){
+                                FinalOutput.auth = "employee";
                                 FinalOutput finalOutput = new FinalOutput();
                                 finalOutput.setVisible(true);
                                 setVisible(false);
@@ -219,6 +315,7 @@ class FinalOutput extends JFrame {
 
                                         if(name.equals(txfAdminName.getText()) && password.equals(new String(txfAdminPass.getPassword()))){
                                             FinalOutput finalOutput = new FinalOutput();
+                                            finalOutput.auth = "admin";
                                             finalOutput.setVisible(true);
                                             setVisible(false);
                                             break;
@@ -227,10 +324,6 @@ class FinalOutput extends JFrame {
                                 } catch (FileNotFoundException ex) {
                                     ex.printStackTrace();
                                 }
-
-//                                FinalOutput finalOutput = new FinalOutput();
-//                                finalOutput.setVisible(true);
-//                                setVisible(false);
                             }
                             break;
                     }
@@ -257,7 +350,73 @@ class FinalOutput extends JFrame {
         }
     }
 
+    public static class RecieptFrame extends JFrame {
+        public static DefaultTableModel receiptTableModel = new DefaultTableModel();
+
+        public RecieptFrame() {
+            setVisible(false);
+            setSize(400, 700);
+            setDefaultCloseOperation(HIDE_ON_CLOSE);
+
+            Container container = getContentPane();
+
+            // Receipt Header
+            JTextPane headerText = new JTextPane();
+            headerText.setText(
+                    "\nJOLLIBEE" +
+                    "\n==Customers Copy==" +
+                    "\nUnknown Place, Dasmariñas City, Cavite" +
+                    "\nTel: ***********" +
+                    "\nCounter #: " +
+                    "\n******************************************\n"
+            );
+            StyledDocument headerDoc = headerText.getStyledDocument();
+            SimpleAttributeSet headerCenter = new SimpleAttributeSet();
+            StyleConstants.setAlignment(headerCenter, StyleConstants.ALIGN_CENTER);
+            headerDoc.setParagraphAttributes(0, headerDoc.getLength(), headerCenter, false);
+
+
+            //Receipt Footer
+            JTextPane footerText = new JTextPane();
+            footerText.setText(
+                    "\n******************************************" +
+                    "\nThis serves as your Official Receipt." +
+                            "\nThank you, and please come again" +
+                            "\nFor feedback, please call us at" +
+                            "\nTel: ***********" +
+                            "\nEmail: feedback.sample@sample.com" +
+                            "\nVisit us also at www.jollibee.com.ph\n"
+            );
+            StyledDocument footerDoc = footerText.getStyledDocument();
+            SimpleAttributeSet footerCenter = new SimpleAttributeSet();
+            StyleConstants.setAlignment(footerCenter, StyleConstants.ALIGN_CENTER);
+            footerDoc.setParagraphAttributes(0, footerDoc.getLength(), footerCenter, false);
+
+
+            //Main Receipt
+            JPanel mainReceiptPanel = new JPanel();
+            mainReceiptPanel.setBackground(Color.white);
+            mainReceiptPanel.setLayout(new BoxLayout(mainReceiptPanel, BoxLayout.Y_AXIS));
+
+            String[] receiptColumnIdentifiers = {"Name", "Total"};
+            receiptTableModel.setColumnIdentifiers(receiptColumnIdentifiers);
+
+            String[] rowData = {"Sample", "2"};
+            receiptTableModel.addRow(rowData);
+
+            JTable receiptTable = new JTable(receiptTableModel);
+
+            mainReceiptPanel.add(new JScrollPane(receiptTable));
+            mainReceiptPanel.add(new JLabel("TOTAL: " + TOTAL));
+
+            container.add(headerText, BorderLayout.NORTH);
+            container.add(mainReceiptPanel, BorderLayout.CENTER);
+            container.add(footerText, BorderLayout.SOUTH);
+        }
+    }
+
     public static void main(String args[]){
         LoginFrame loginFrame = new LoginFrame();
+//        RecieptFrame recieptFrame = new RecieptFrame();
     }
 }
